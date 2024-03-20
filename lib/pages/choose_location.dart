a6056f27e38db97fe1144768b626fc3570433828
bekatadesse <bekatadesse5@gmail.com>
2024-03-20 17:10:03 +0300

    create home page

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  int countr= 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState function ran');
+  }
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        title: const Text('Choose a Location',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.white,),),
+        centerTitle: true,
+        ),
+      body: ElevatedButton.icon(
+        onPressed: (){
+          setState(() {
+            countr += 1;
+          });
+        },
+        icon: Icon(Icons.add_location),
+        label: Text('Add Location'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 13.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/choose_location');
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue[400]),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.edit_location), // Your desired icon
+                  SizedBox(width: 5.0), // Add some spacing
+                  Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
